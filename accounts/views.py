3cd44c6a716d53f54aac739ea8f9d72f6f2b0a28
srikant132 <srikantcs132@gmail.com>
2018-12-15 18:49:14 +0530

    Authentication related stuff

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def signup(request):
-    render(request,'accounts/signup.html')
+    if request.method == 'POST':
+        #Usert has info and wants an accounts now!
+        if request.POST['password1'] == request.POST['password2']:
+           try:
+               user = User.objects.get(username=request.POST['username'])
+               return render(request,'accounts/signup.html',{'error':'Username is all ready taken'})
+           except User.DoesNotExist:
+                user= User.objects.create_user(request.POST['username'],password=request.POST['password1'])
+                auth.login(request,user)
+                return redirect('home')
+                
+    else:
+        #user want to enter info
+        return render(request,'accounts/signup.html')
 
 
 def login(request):
-    render(request,'accounts/login.html')
+    return render(request,'accounts/login.html')
 
 
 def logout(request):
-    render(request,'accounts/logout.html')
+    # TODO  need to route to homepage
+    # and dont forget to logout
+    return render(request,'accounts/signup.html')
