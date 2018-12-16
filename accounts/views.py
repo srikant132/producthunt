06abc1040d94384567da7fa80bb38626d7a539cb
srikant132 <srikantcs132@gmail.com>
2018-12-16 14:46:57 +0530

    writing condition to login and logout

--- accounts/views.py
+++ accounts/views.py
@@ -22,10 +22,19 @@ def signup(request):
 
 
 def login(request):
-    return render(request,'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            return render(request,'accounts/login.html',{'error':'username or password is incorrect.'})
+
+    else:
+        return render(request,'accounts/login.html')
 
 
 def logout(request):
-    # TODO  need to route to homepage
-    # and dont forget to logout
-    return render(request,'accounts/signup.html')
+    if request.method == 'POST':
+       auth.logout(request)
+       return redirect('home')
