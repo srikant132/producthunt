4ca37d58a9d0f707612d16719497d018e9a4f389
srikant132 <srikantcs132@gmail.com>
2018-12-16 15:28:47 +0530

    litle changes in import files

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+
 
 # Create your views here.
 def signup(request):
