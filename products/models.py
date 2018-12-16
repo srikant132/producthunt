75c380b55c051663eacfc5428e92f8c49085f09c
srikant132 <srikantcs132@gmail.com>
2018-12-16 17:22:59 +0530

    here model name miss match

--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class product(models.Model):
+class Product(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
