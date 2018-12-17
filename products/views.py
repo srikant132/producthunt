7e51ab4a49986b694bdf554490abfe1d28d4c4c2
srikant132 <srikantcs132@gmail.com>
2018-12-17 13:13:13 +0530

    it send to signup page when we try to vote without register

--- products/views.py
+++ products/views.py
@@ -4,9 +4,10 @@ from .models import Product
 from django.utils import timezone
 # Create your views here.
 def home(request):
-    return render(request,'products/home.html')
+    products = Product.objects
+    return render(request,'products/home.html',{'products':products})
 
-@login_required
+@login_required(login_url="/accounts/signup")
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
@@ -37,7 +38,7 @@ def detail(request,product_id):
     product= get_object_or_404(Product,pk=product_id)
     return render(request,'products/detail.html',{'product':product})
 
-@login_required
+@login_required(login_url="/accounts/signup")
 def upvote(request,product_id):
     if request.method == 'POST':
         product= get_object_or_404(Product,pk=product_id)
